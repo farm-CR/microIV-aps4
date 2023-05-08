b242649169694e9d2d1cab5ba7313b7117b2b4b1
Julio-Mugnol <jcmugnol@gmail.com>
2023-05-08 15:28:45 -0300

    New way to get PNAD. Updating analysis

--- aps4.R
+++ aps4.R
@@ -2,45 +2,42 @@ library(tidyverse)
 library(survey)
 library(MatchIt)
 
-load("pnad_d.RData")
-load("pnad_p.RData")
+load("pnad.RData")
+load("dom.RData")
 
-pnad_d <- pnad_d %>% 
-  filter(!is.na(renda_mensal_domiciliar_compativel_1992_deflacionado)) %>% 
-  mutate(renda_per_capita = renda_mensal_domiciliar_compativel_1992_deflacionado / total_pessoas) %>% 
+dom <- dom %>% 
+  filter(!is.na(renda_mensal)) %>% 
+  mutate(renda_per_capita = renda_mensal / total_moradores) %>% 
   slice_min(order_by = renda_per_capita, prop = 0.2)
 
-domicilios <- pnad_p %>% 
-  semi_join(pnad_d, by = "id_domicilio") %>% 
+domicilios <- pnad %>% 
+  filter(ano == 2006 | ano == 2009) %>% 
+  semi_join(dom, by = "id_domicilio") %>% 
   filter(idade %in% c(14, 15, 16, 17)) %>%
   mutate(g_idade = ifelse(idade == 14 | idade == 15, "BVJ", "nBVJ")) %>% 
-  group_by(id_domicilio, numero_familia, ano, g_idade) %>% 
+  group_by(id_domicilio, n_familia, ano, g_idade) %>% 
   summarise(n = n()) %>% 
   pivot_wider(names_from = g_idade, values_from = n) %>% 
   mutate(controle = nBVJ == 2 & is.na(BVJ),
          tratamento = BVJ == 1 & nBVJ == 1) %>% 
   filter(controle | tratamento) %>% 
-  select(id_domicilio, ano, numero_familia, tratamento)
+  select(id_domicilio, ano, n_familia, tratamento)
 
-pnad_p <- pnad_p %>% 
+pnad <- pnad %>% 
   filter(idade %in% 14:17) %>% 
-  inner_join(domicilios, by = c("id_domicilio", "ano", "numero_familia")) %>% 
-  inner_join(pnad_d, by = c("id_domicilio", "ano"))
-  # select(id_domicilio, ano, numero_familia, tratamento, id_regiao = id_regiao.x, 
-  #        id_uf = id_uf.x, sexo, idade, raca_cor, frequenta_escola, renda_per_capita, tinha_outro_trabalho,
-  #        peso_amostral) %>% 
+  inner_join(domicilios, by = c("id_domicilio", "ano", "n_familia")) %>% 
+  inner_join(dom, by = c("id_domicilio", "ano", "uf"))
 
-pnad_p %>% 
-  group_by(total_pessoas) %>% 
+pnad %>% 
+  group_by(tratamento, ano) %>% 
   count()
 
 svy <- svydesign(id = ~1,
-                 strata = ~id_uf.x,
-                 weights = ~peso_amostral, 
-                 data = pnad_p %>% filter(ano == 2009))
+                 strata = ~STRAT,
+                 weights = ~peso, 
+                 data = pnad %>% filter(ano == 2009))
 
-psm <- svyglm(as.integer(frequenta_escola) ~ sexo + raca_cor + trabalhou_semana +
-                renda_per_capita + total_pessoas,
+psm <- svyglm(as.integer(frequenta_escola) ~ sexo + raca,
               design = svy,
               family = binomial(link = "logit"))
 psm <- glm(as.integer(frequenta_escola) ~ sexo + raca_cor + trabalhou_semana +
@@ -48,28 +45,25 @@ psm <- glm(as.integer(frequenta_escola) ~ sexo + raca_cor + trabalhou_semana +
            family = binomial(link = "logit"),
            data = pnad_p %>% filter(ano == 2009))
 
-psm <- glm(as.integer(tratamento) ~ sexo + raca_cor + trabalhou_semana +
-             renda_per_capita + total_pessoas,
+psm <- glm(as.integer(tratamento) ~ sexo + raca + mae_viva + nasceu_mun + 
+             nasceu_uf + le_escreve + trabalhou_semana + total_moradores,
            family = binomial(link = "logit"),
-           data = pnad_p %>% filter(ano == 2009))
-
+           data = pnad %>% filter(ano == 2009))
 summary(psm)
 pscore <- predict(psm, type="response")
 
-ggplot(pnad_p %>% filter(ano == 2009), mapping = aes(pscore)) + 
+ggplot(pnad %>% filter(ano == 2009), mapping = aes(pscore)) + 
   geom_density() +
   facet_wrap(~tratamento) +
   xlab("p")
 
-match_ATT <- matchit(as.integer(tratamento) ~ as.factor(sexo) + as.factor(raca_cor) + 
-                       trabalhou_semana + total_pessoas,
-                     data=pnad_p %>% filter(ano == 2009),
-                     link="logit",
-                     method="full",
-                     discard="both")
+match_ATT <- matchit(as.integer(tratamento) ~ sexo + raca + mae_viva + nasceu_mun + 
+                       nasceu_uf + le_escreve + trabalhou_semana + total_moradores,
+                     data = pnad %>% filter(ano == 2009),
+                     link = "logit",
+                     method = "full",
+                     discard = "both")
 summary(match_ATT)
 
 plot(match_ATT,type="density",interactive=FALSE)
 
-
-# pnad_d <- read.fwf(file = "PNAD/DOM2006.txt", widths = dicdom2009$tamanho, fileEncoding="latin1")
--- query_pnad.R
+++ query_pnad.R
@@ -1,18 +1,93 @@
-library(basedosdados)
 library(tidyverse)
 
-set_billing_id("insper-385811")
+pnad03 <- readRDS("PNAD/pnad-2003.rds") %>% as_tibble()
+pnad06 <- readRDS("PNAD/pnad-2006.rds") %>% as_tibble()
+pnad09 <- readRDS("PNAD/pnad-2009.rds") %>% as_tibble()
 
-query <- bdplyr("br_ibge_pnad.microdados_compatibilizados_domicilio")
-query <- query %>% 
-  filter(ano %in% c(2003, 2006, 2009))
+pnad03 <- pnad03 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, sexo = v0302, idade = v8005,
+         n_familia = v0403, raca = v0404, mae_viva = v0405, nasceu_mun = v0501, nasceu_uf = v0502,
+         le_escreve = v0601, frequenta_escola = v0602, trabalhou_semana = v9001) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""),
+         nasceu_uf = replace_na(nasceu_uf, 0),
+         frequenta_escola = replace_na(frequenta_escola, 0),
+         trabalhou_semana = replace_na(trabalhou_semana, 0),
+         across(c(sexo, raca, mae_viva, nasceu_mun, nasceu_uf, le_escreve, frequenta_escola, trabalhou_semana),
+                as_factor),
+         sexo = fct_recode(sexo, "M" = "2", "F" = "4"),
+         raca = fct_collapse(raca, "bra" = c("2", "6"), "pre" = c("4", "8", "0"), "nd" = "9"),
+         mae_viva = fct_recode(mae_viva, "S" = "1", "N" = "3", "ns" = "5", "nd" = "9"),
+         nasceu_mun = fct_recode(nasceu_mun, "S" = "1", "N" = "3", "nd" = "0"),
+         nasceu_uf = fct_recode(nasceu_uf, "S" = "2", "N" = "4", "nd" = "0"),
+         le_escreve = fct_recode(le_escreve, "S" = "1", "N" = "3", "nd" = "0"),
+         frequenta_escola = fct_recode(frequenta_escola, "S" = "2", "N" = "4", "nd" = "0"),
+         trabalhou_semana = fct_recode(trabalhou_semana, "S" = "1", "N" = "3", "nd" = "0"))
+  
 
-pnad_d <- query %>% bd_collect(.)
-save(pnad_d, file = "pnad_d.RData")
+pnad06 <- pnad06 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, sexo = v0302, idade = v8005,
+         n_familia = v0403, raca = v0404, mae_viva = v0405, nasceu_mun = v0501, nasceu_uf = v0502,
+         le_escreve = v0601, frequenta_escola = v0602, trabalhou_semana = v9001) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""),
+         nasceu_uf = replace_na(nasceu_uf, 0),
+         frequenta_escola = replace_na(frequenta_escola, 0),
+         trabalhou_semana = replace_na(trabalhou_semana, 0),
+         across(c(sexo, raca, mae_viva, nasceu_mun, nasceu_uf, le_escreve, frequenta_escola, trabalhou_semana),
+                as_factor),
+         sexo = fct_recode(sexo, "M" = "2", "F" = "4"),
+         raca = fct_collapse(raca, "bra" = c("2", "6"), "pre" = c("4", "8", "0"), "nd" = "9"),
+         mae_viva = fct_recode(mae_viva, "S" = "1", "N" = "3", "ns" = "5", "nd" = "9"),
+         nasceu_mun = fct_recode(nasceu_mun, "S" = "1", "N" = "3", "nd" = "0"),
+         nasceu_uf = fct_recode(nasceu_uf, "S" = "2", "N" = "4", "nd" = "0"),
+         le_escreve = fct_recode(le_escreve, "S" = "1", "N" = "3", "nd" = "0"),
+         frequenta_escola = fct_recode(frequenta_escola, "S" = "2", "N" = "4", "nd" = "0"),
+         trabalhou_semana = fct_recode(trabalhou_semana, "S" = "1", "N" = "3", "nd" = "0"))
 
-query <- bdplyr("br_ibge_pnad.microdados_compatibilizados_pessoa")
-query <- query %>% 
-  filter(ano %in% c(2003, 2006, 2009))
+pnad09 <- pnad09 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, sexo = v0302, idade = v8005,
+         n_familia = v0403, raca = v0404, mae_viva = v0405, nasceu_mun = v0501, nasceu_uf = v0502,
+         le_escreve = v0601, frequenta_escola = v0602, trabalhou_semana = v9001) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""),
+         nasceu_uf = replace_na(nasceu_uf, 0),
+         frequenta_escola = replace_na(frequenta_escola, 0),
+         trabalhou_semana = replace_na(trabalhou_semana, 0),
+         across(c(sexo, raca, mae_viva, nasceu_mun, nasceu_uf, le_escreve, frequenta_escola, trabalhou_semana),
+                as_factor),
+         sexo = fct_recode(sexo, "M" = "2", "F" = "4"),
+         raca = fct_collapse(raca, "bra" = c("2", "6"), "pre" = c("4", "8", "0"), "nd" = "9"),
+         mae_viva = fct_recode(mae_viva, "S" = "1", "N" = "3", "ns" = "5"),
+         nasceu_mun = fct_recode(nasceu_mun, "S" = "1", "N" = "3", "nd" = "0"),
+         nasceu_uf = fct_recode(nasceu_uf, "S" = "2", "N" = "4", "nd" = "0"),
+         le_escreve = fct_recode(le_escreve, "S" = "1", "N" = "3", "nd" = "0"),
+         frequenta_escola = fct_recode(frequenta_escola, "S" = "2", "N" = "4", "nd" = "0"),
+         trabalhou_semana = fct_recode(trabalhou_semana, "S" = "1", "N" = "3", "nd" = "0"))
 
-pnad_p <- query %>% bd_collect(.)
-save(pnad_p, file = "pnad_p.RData")
+pnad <- pnad03 %>% bind_rows(pnad06, pnad09)
+save(pnad, file = "pnad.RData")
+
+
+dom03 <- readRDS("PNAD/dom2003.rds") %>% as_tibble()
+dom06 <- readRDS("PNAD/dom2006.rds") %>% as_tibble()
+dom09 <- readRDS("PNAD/dom2009.rds") %>% as_tibble()
+
+dom03 <- dom03 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, total_moradores = v0105,
+         estrato = v4602, peso = v4611, renda_mensal = v4614,
+         UPA = v4615, STRAT = v4617, PSU = v4618) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""))
+
+dom06 <- dom06 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, total_moradores = v0105,
+         estrato = v4602, peso = v4611, renda_mensal = v4614,
+         UPA, STRAT = v4617, PSU = v4618) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""))
+
+dom09 <- dom09 %>% 
+  select(ano = v0101, uf, n_controle = v0102, n_serie = v0103, total_moradores = v0105,
+         estrato = v4602, peso = v4611, renda_mensal = v4614,
+         UPA, STRAT = v4617, PSU = v4618) %>% 
+  mutate(id_domicilio = paste(ano, n_controle, n_serie, sep = ""))
+
+dom <- dom03 %>% bind_rows(dom06, dom09)
+save(dom, file = "dom.RData")
+
